5a3538804c59c9c9c5e15fb3b5370ce675fafccb
m-shimao <shimao.ma@gmail.com>
2018-08-10 14:28:27 +0900

    :shirt:インデントを修正

--- aws/spot_instance/ml/main.tf
+++ aws/spot_instance/ml/main.tf
@@ -1,15 +1,16 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region = "${var.region}"
+  region     = "${var.region}"
 }
 
 # VPC
 resource "aws_vpc" "ml-vpc" {
-  cidr_block = "10.1.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_support = "true"
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
   enable_dns_hostnames = "true"
+
   tags {
     Name = "ml-vpc"
   }
@@ -18,6 +19,7 @@ resource "aws_vpc" "ml-vpc" {
 # Internet Gateway
 resource "aws_internet_gateway" "ml-igw" {
   vpc_id = "${aws_vpc.ml-vpc.id}"
+
   tags {
     Name = "ml-igw"
   }
@@ -25,11 +27,12 @@ resource "aws_internet_gateway" "ml-igw" {
 
 # Subnet
 resource "aws_subnet" "ml-subnet-public" {
-  count = "${length(var.availability_zones)}"
-  vpc_id = "${aws_vpc.ml-vpc.id}"
-  cidr_block = "${format("10.1.%d.0/24", count.index + 1)}"
-  availability_zone = "${lookup(var.availability_zones, count.index)}"
+  count                   = "${length(var.availability_zones)}"
+  vpc_id                  = "${aws_vpc.ml-vpc.id}"
+  cidr_block              = "${format("10.1.%d.0/24", count.index + 1)}"
+  availability_zone       = "${lookup(var.availability_zones, count.index)}"
   map_public_ip_on_launch = "true"
+
   tags {
     Name = "${format("ml-subnet-public-%d", count.index + 1)}"
   }
@@ -38,38 +41,44 @@ resource "aws_subnet" "ml-subnet-public" {
 # Route Table
 resource "aws_route_table" "ml-route-public" {
   vpc_id = "${aws_vpc.ml-vpc.id}"
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.ml-igw.id}"
   }
+
   tags {
     Name = "ml-route-table-public"
   }
 }
+
 resource "aws_route_table_association" "ml-assoc" {
-  count = "${length(var.availability_zones)}"
-  subnet_id = "${element(aws_subnet.ml-subnet-public.*.id, count.index)}"
+  count          = "${length(var.availability_zones)}"
+  subnet_id      = "${element(aws_subnet.ml-subnet-public.*.id, count.index)}"
   route_table_id = "${aws_route_table.ml-route-public.id}"
 }
 
 # Security Group
 ### Web
 resource "aws_security_group" "ml-web-sg" {
-  name = "ml-web-sg"
+  name        = "ml-web-sg"
   description = "Allow SSH inbound traffic"
-  vpc_id = "${aws_vpc.ml-vpc.id}"
+  vpc_id      = "${aws_vpc.ml-vpc.id}"
+
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["${var.my_ip_address}"]
   }
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "ml-web-sg"
   }
@@ -77,7 +86,7 @@ resource "aws_security_group" "ml-web-sg" {
 
 # Key Pair
 resource "aws_key_pair" "ml-key" {
-  key_name = "ml-key"
+  key_name   = "ml-key"
   public_key = "${var.aws_public_key}"
 }
 
@@ -85,53 +94,62 @@ data "aws_caller_identity" "current" {}
 
 # Spot Fleet Request
 resource "aws_spot_fleet_request" "ml-spot-request" {
-  iam_fleet_role  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-ec2-spot-fleet-tagging-role"
+  iam_fleet_role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-ec2-spot-fleet-tagging-role"
+
   # spot_price      = "0.1290" # Max Price デフォルトはOn-demand Price
-  target_capacity = "${var.spot_target_capacity}"
+  target_capacity                     = "${var.spot_target_capacity}"
   terminate_instances_with_expiration = true
-  wait_for_fulfillment = "true" # fulfillするまでTerraformが待つ
+  wait_for_fulfillment                = "true"                        # fulfillするまでTerraformが待つ
 
   launch_specification {
-    ami = "${var.spot_instance_ami}"
-    instance_type = "${var.spot_instance_type}"
-    key_name = "${aws_key_pair.ml-key.key_name}"
-    vpc_security_group_ids = ["${aws_security_group.ml-web-sg.id}"]
-    subnet_id = "${element(aws_subnet.ml-subnet-public.*.id, 0)}"
+    ami                         = "${var.spot_instance_ami}"
+    instance_type               = "${var.spot_instance_type}"
+    key_name                    = "${aws_key_pair.ml-key.key_name}"
+    vpc_security_group_ids      = ["${aws_security_group.ml-web-sg.id}"]
+    subnet_id                   = "${element(aws_subnet.ml-subnet-public.*.id, 0)}"
     associate_public_ip_address = true
+
     root_block_device {
       volume_size = "${var.gp2_volume_size}"
       volume_type = "gp2"
     }
+
     tags {
       Name = "ml-instance"
     }
   }
+
   launch_specification {
-    ami = "${var.spot_instance_ami}"
-    instance_type = "${var.spot_instance_type}"
-    key_name = "${aws_key_pair.ml-key.key_name}"
-    vpc_security_group_ids = ["${aws_security_group.ml-web-sg.id}"]
-    subnet_id = "${element(aws_subnet.ml-subnet-public.*.id, 1)}"
+    ami                         = "${var.spot_instance_ami}"
+    instance_type               = "${var.spot_instance_type}"
+    key_name                    = "${aws_key_pair.ml-key.key_name}"
+    vpc_security_group_ids      = ["${aws_security_group.ml-web-sg.id}"]
+    subnet_id                   = "${element(aws_subnet.ml-subnet-public.*.id, 1)}"
     associate_public_ip_address = true
+
     root_block_device {
       volume_size = "${var.gp2_volume_size}"
       volume_type = "gp2"
     }
+
     tags {
       Name = "ml-instance"
     }
   }
+
   launch_specification {
-    ami = "${var.spot_instance_ami}"
-    instance_type = "${var.spot_instance_type}"
-    key_name = "${aws_key_pair.ml-key.key_name}"
-    vpc_security_group_ids = ["${aws_security_group.ml-web-sg.id}"]
-    subnet_id = "${element(aws_subnet.ml-subnet-public.*.id, 2)}"
+    ami                         = "${var.spot_instance_ami}"
+    instance_type               = "${var.spot_instance_type}"
+    key_name                    = "${aws_key_pair.ml-key.key_name}"
+    vpc_security_group_ids      = ["${aws_security_group.ml-web-sg.id}"]
+    subnet_id                   = "${element(aws_subnet.ml-subnet-public.*.id, 2)}"
     associate_public_ip_address = true
+
     root_block_device {
       volume_size = "${var.gp2_volume_size}"
       volume_type = "gp2"
     }
+
     tags {
       Name = "ml-instance"
     }
@@ -143,10 +161,11 @@ data "aws_instance" "ml-instance" {
     name   = "tag:Name"
     values = ["ml-instance"]
   }
+
   depends_on = ["aws_spot_fleet_request.ml-spot-request"]
 }
 
 output "ip" {
-  value = "${data.aws_instance.ml-instance.public_ip}"
+  value      = "${data.aws_instance.ml-instance.public_ip}"
   depends_on = ["aws_spot_fleet_request.ml-spot-request"]
 }
--- aws/spot_instance/ml/variables.tf
+++ aws/spot_instance/ml/variables.tf
@@ -2,9 +2,11 @@ variable "my_ip_address" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_public_key" {}
+
 variable "region" {
   default = "us-west-2"
 }
+
 variable "availability_zones" {
   default = {
     "0" = "us-west-2a"
@@ -12,15 +14,19 @@ variable "availability_zones" {
     "2" = "us-west-2c"
   }
 }
+
 variable "spot_instance_ami" {
   default = "ami-0faada77" # Deep Learning AMI (Ubuntu) Version 9.0
 }
+
 variable "spot_instance_type" {
   default = "p2.xlarge"
 }
+
 variable "spot_target_capacity" {
   default = "1"
 }
+
 variable "gp2_volume_size" {
   default = "100"
 }
